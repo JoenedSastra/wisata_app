5f743673bb25cafdb37bc61ff440027209b3648e
JoenedSastra <joenedsastra812@gmail.com>
2026-06-20 03:56:59 +0700

    test php

--- api/index.php
+++ api/index.php
@@ -1,28 +1,4 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../public';
-
-$directories = [
-    '/tmp/storage/app/public',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/logs',
-];
-
-foreach ($directories as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-$app->useStoragePath('/tmp/storage');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// Test apakah PHP jalan
+echo "PHP OK";
